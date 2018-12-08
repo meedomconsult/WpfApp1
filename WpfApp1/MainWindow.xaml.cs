cfaeb4f7787a85ee389d1ce956f5bc4a98df6d80
Jørgen Meedom <jmeedom@gmail.com>
2018-12-08 23:48:35 +0100

    Everything

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Enter password");
+        }
+    }
+}
