9be9190c43ab8d737efcc44d0310e42825707a25
Jørgen Meedom <jmeedom@gmail.com>
2018-12-09 01:40:47 +0100

    Added tests

---
+++ WpfApp1Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1Tests
+{
+    public class WpfApp1Test
+    {
+        [Test]
+        public void App1Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
